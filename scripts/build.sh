c969b1957f6604141c05349920657aa7548cbb90
the1codemaster <the1codemaster@gmail.com>
2018-10-13 21:51:11 -0700

    minor fix to the generating script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-template_nav="<a href="/">Home</a>"
+template_nav=$(cat <<EOF | sed 's/^/  /'
+<a href="/">Home</a>
+EOF
+)
+
+# Out with the old...
+rm -rf docs
 
 # Loop through all posts and generate html documents
 
 for filename in posts/*.tex; do
     sanitized=$(echo "$filename" | awk -F '/' '{print $NF}' \
                     | tr '[:upper:]' '[:lower:]' \
-                   | sed 's/ /-/g')
+                    | sed 's/ /-/g')
     url=$(basename $sanitized .tex)
 
     echo "Writing $url..."
@@ -18,12 +24,14 @@ for filename in posts/*.tex; do
     pandoc $filename -s --mathjax -o docs/$url.html 
 
     # Remove everything but the body
-    contents=$(cat docs/$url.html | sed -n '/<body>/,/<\/body>/p' | sed '/body/d' | sed 's/^/ /')
+    contents=$(cat docs/$url.html | sed -n '/<body>/,/<\/body>/p' | sed '/body/d' | sed 's/^/  /')
 
     cat <<EOF | mustache - template.html.ms > docs/$url/index.html
 ---
 content: | 
 $contents 
+nav: |
+$template_nav
 ---
 EOF
 done
